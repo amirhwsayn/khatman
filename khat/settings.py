807f076e0ca478be82b003c1bab88f382eb43033
Vitor Freitasamirhwsayn <vitor@simpleisbetterthancomplex>
2020-11-24 07:05:57 +0330

    sfgdsfhdfh

--- khat/settings.py
+++ khat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
